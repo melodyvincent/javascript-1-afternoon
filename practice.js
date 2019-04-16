57784a901cc82caa61347c669c9dd39b6ab7297e
Melody Vincent <melodyvincent@Melodys-MacBook-Pro.local>
2019-04-16 14:16:48 -0600

    added new greeting

--- practice.js
+++ practice.js
@@ -36,7 +36,10 @@ var devMountainGoal = null
 //greeting should return the string "Hello, "
 //plus the value of the name parameter.
 
-//Code here
+function greeting (name) {
+  return "Hello, " + name
+  
+}
 
 
 //////////////////PROBLEM 7////////////////////
