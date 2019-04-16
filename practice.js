b71d602f47f38bcd89be68c77e0f3b7085327d36
Melody Vincent <melodyvincent@Melodys-MacBook-Pro.local>
2019-04-16 14:10:37 -0600

    added greatest fear

--- practice.js
+++ practice.js
@@ -9,25 +9,25 @@ var myName = "Melody"
 
 //Create a variable called myAge that is a number data type
 
-//Code here
+var myAge = 15
 
 //////////////////PROBLEM 3////////////////////
 
 //Create a variable called lovesCode that is a boolean data type
 
-//Code here
+var lovesCode = true
 
 //////////////////PROBLEM 4////////////////////
 
 //Create a variable called greatestFear that is undefined because we fear nothing
 
-//Code here
+var greatestFear 
 
 //////////////////PROBLEM 5////////////////////
 
 //Create a variable called devMountainGoal that is null because we are just starting out
 
-//Code here
+var devMountainGoal = null
 
 //////////////////PROBLEM 6////////////////////
 
